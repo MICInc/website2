ed9165fd1b9d486df3fa6f7cf5c4a84de2451973
ch3njust1n <ch3njus@gmail.com>
2020-03-07 15:22:58 -0500

    Implemented basic mobile menu

--- script/main.js
+++ script/main.js
@@ -1,3 +1,14 @@
-function toggle_pancake(x) {
-	x.classList.toggle("change");
+function toggle_pancake() {
+	var x = document.getElementById("menu");
+	var nav = document.getElementById("main-nav");
+	if (x.style.display === "none") {
+		x.style.display = "block";
+		nav.style.height = "100%";
+		nav.style.backgroundColor = "#FFF";
+	} 
+	else {
+		x.style.display = "none";
+		nav.style.height = "auto";
+		nav.style.backgroundColor = "transparent";
+	}
 }
