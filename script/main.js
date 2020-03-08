3075405ab664dbba338d188431e6cd7d0434b831
ch3njust1n <ch3njus@gmail.com>
2020-03-07 20:32:06 -0500

    Saving progress on pancake menu. Switch gets confused on first click and switches pancake with X and vice versa.

--- script/main.js
+++ script/main.js
@@ -1,6 +1,9 @@
 function toggle_pancake() {
-	var x = document.getElementById("menu");
+	document.getElementById("pancake").classList.toggle("change");
 	var nav = document.getElementById("main-nav");
+	var x = document.getElementById("menu");
+	x.classList.toggle("change");
+	
 	if (x.style.display === "none") {
 		x.style.display = "block";
 		nav.style.height = "100%";
