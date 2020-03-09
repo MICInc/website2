14393504fe0102e09c40914514171e4733a929e9
ch3njust1n <ch3njus@gmail.com>
2020-03-08 22:19:58 -0400

    Fixed pancake in mobie

--- script/main.js
+++ script/main.js
@@ -1,18 +1,17 @@
 function toggle_pancake() {
-	document.getElementById("pancake").classList.toggle("change");
-	var nav = document.getElementById("main-nav");
-	var x = document.getElementById("menu");
 	var body = document.body;
-	x.classList.toggle("change");
+	var nav = document.getElementById("main-nav");
+	var menu = document.getElementById("menu");
+	document.getElementById("pancake").classList.toggle("change");
 
-	if (x.style.display === "none") {
-		x.style.display = "block";
+	if (menu.style.display !== "block") {
+		menu.style.display = "block";
 		nav.style.height = "100%";
 		nav.style.backgroundColor = "#FFF";
 		body.style.overflow = "hidden";
 	} 
 	else {
-		x.style.display = "none";
+		menu.style.display = "none";
 		nav.style.height = "auto";
 		nav.style.backgroundColor = "transparent";
 		body.style.overflow = "scroll";
