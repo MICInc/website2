2fc3899c4b2c2db3408d85dfc00aa2a139a1a0e6
ch3njust1n <ch3njus@gmail.com>
2020-05-01 00:25:45 -0400

    Added ImageSect which will sit between text sections and have images that either take the entire width or just a column.

---
+++ src/components/ImageSect.vue
@@ -0,0 +1,34 @@
+<template>
+	<div id="about" class="sect">
+		<div class="content">
+			<img class="crop" src="assets/img/kenneth.jpg">
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'imagesect'
+	}
+</script>
+
+<style scoped>
+.content {
+	display:flex;
+}
+
+.crop {
+  width: 300px;
+  height: auto;
+  clip-path: circle();
+}
+
+@media screen and (min-width: 1172px) {
+}
+
+@media screen and (max-width: 360px) {
+	.sect {
+		display: inline-block;
+	}
+}
+</style>
