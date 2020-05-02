2f8ec619a72f7ebf6bb4632c86d3624c68008d12
ch3njust1n <ch3njus@gmail.com>
2020-05-01 22:18:58 -0400

    Fixed conference header style and text centered for mobile 360

--- src/components/Conference.vue
+++ src/components/Conference.vue
@@ -5,18 +5,18 @@
 			<a href="/register"><button class="btn">register</button></a>
 		</div>
 		<div class="content">
-		<p>
-			Lorem ipsum dolor sit amet, ac in eu, dictum rerum a libero. Ac amet. Placerat lorem velit id lorem sit, eros at eget enim, vestibulum diam imperdiet vitae suscipit nonummy, tempus sed consequat enim ligula sit at, fusce justo cras nulla et. Non sit mauris mi in, sed nullam, proin lectus erat. Semper nostra in mi adipiscing at, natoque in, sed nunc. Donec turpis mauris elit morbi praesent mauris, pharetra pharetra. Velit adipiscing in gravida tellus faucibus, tempus cras, risus tellus id lacus. Suspendisse morbi mattis sed convallis ac, leo ut mauris nunc, justo id gravida turpis purus necessitatibus auctor, nunc neque. Pede sed est eleifend quisque, ultrices donec integer suscipit enim gravida ornare, laoreet elementum, urna est euismod risus massa tortor.
-		</p>
-		<p>
-			Fusce enim metus ultrices donec, donec magna. Consectetuer vero ac sapien, elit leo vel, dictum egestas. Tempor erat ac. Amet adipiscing rutrum in erat ut, mauris ante, integer mauris netus, felis ultrices in dolor pede turpis. Non vestibulum viverra vestibulum in, nam et dolor mi, sodales posuere arcu non vestibulum et, adipiscing lacus quam proin quisque.
-		</p>
-		<p>
-			Voluptatem atque proin ut sit repudiandae. Nulla sit. Erat enim sed amet elit vestibulum, porta ut diam etiam maecenas wisi at, condimentum eget a, quis dui quis, turpis quidem id ut. Sed sem ac, a lectus pede sed. Mattis donec lorem id bibendum consequat, sed justo convallis arcu interdum lectus, cras ut semper proin sed vestibulum.
-    </p>
-		<p>
-			Magna nec convallis ultrices eu sem, ante sem morbi lorem mauris tortor dui, fermentum ut. Vitae metus orci, nulla purus, dapibus nunc. Fames felis libero nulla leo natoque amet, viverra nunc, tellus nam sociis, sed interdum viverra dolor risus voluptas. Mauris vulputate, arcu volutpat lectus fermentum ornare ut id, a pellentesque. A amet augue natoque, nullam velit amet, vestibulum nulla exercitation. Libero eros lectus. Nec a libero, sed phasellus lorem justo ultrices proin etiam, quibusdam nunc. Massa proin hendrerit sapien adipiscing luctus tristique, nulla cursus imperdiet convallis.
-		</p>
+			<p>
+				Lorem ipsum dolor sit amet, ac in eu, dictum rerum a libero. Ac amet. Placerat lorem velit id lorem sit, eros at eget enim, vestibulum diam imperdiet vitae suscipit nonummy, tempus sed consequat enim ligula sit at, fusce justo cras nulla et. Non sit mauris mi in, sed nullam, proin lectus erat. Semper nostra in mi adipiscing at, natoque in, sed nunc. Donec turpis mauris elit morbi praesent mauris, pharetra pharetra. Velit adipiscing in gravida tellus faucibus, tempus cras, risus tellus id lacus. Suspendisse morbi mattis sed convallis ac, leo ut mauris nunc, justo id gravida turpis purus necessitatibus auctor, nunc neque. Pede sed est eleifend quisque, ultrices donec integer suscipit enim gravida ornare, laoreet elementum, urna est euismod risus massa tortor.
+			</p>
+			<p>
+				Fusce enim metus ultrices donec, donec magna. Consectetuer vero ac sapien, elit leo vel, dictum egestas. Tempor erat ac. Amet adipiscing rutrum in erat ut, mauris ante, integer mauris netus, felis ultrices in dolor pede turpis. Non vestibulum viverra vestibulum in, nam et dolor mi, sodales posuere arcu non vestibulum et, adipiscing lacus quam proin quisque.
+			</p>
+			<p>
+				Voluptatem atque proin ut sit repudiandae. Nulla sit. Erat enim sed amet elit vestibulum, porta ut diam etiam maecenas wisi at, condimentum eget a, quis dui quis, turpis quidem id ut. Sed sem ac, a lectus pede sed. Mattis donec lorem id bibendum consequat, sed justo convallis arcu interdum lectus, cras ut semper proin sed vestibulum.
+	    </p>
+			<p>
+				Magna nec convallis ultrices eu sem, ante sem morbi lorem mauris tortor dui, fermentum ut. Vitae metus orci, nulla purus, dapibus nunc. Fames felis libero nulla leo natoque amet, viverra nunc, tellus nam sociis, sed interdum viverra dolor risus voluptas. Mauris vulputate, arcu volutpat lectus fermentum ornare ut id, a pellentesque. A amet augue natoque, nullam velit amet, vestibulum nulla exercitation. Libero eros lectus. Nec a libero, sed phasellus lorem justo ultrices proin etiam, quibusdam nunc. Massa proin hendrerit sapien adipiscing luctus tristique, nulla cursus imperdiet convallis.
+			</p>
 		</div>
 		<div class="committee">
 			<ul>
@@ -94,9 +94,14 @@
 }
 
 @media screen and (max-width: 360px) {
+	.compress-header {
+			width: 100%;
+	}
+
 	.header-sect {
 		display: flex;
 		flex-direction: column;
+		margin-bottom: 20px;
 	}
 
 	.content {
@@ -106,6 +111,11 @@
 
 	.content ul {
 		width: 100%;
+	}
+
+	p {
+		text-align: center;
+		margin-bottom: 10px;
 	}
 
 	.sect {
