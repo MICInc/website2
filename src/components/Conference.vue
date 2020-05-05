eca94f47cf7da9d6aea134c7fa6a9936f12fc415
ch3njust1n <ch3njus@gmail.com>
2020-05-04 23:55:04 -0400

    Adjusted conference 992 breakpoint

--- src/components/Conference.vue
+++ src/components/Conference.vue
@@ -240,11 +240,20 @@
 
 /* Large devices (laptops/desktops, 992px and up) */
 @media only screen and (min-width: 992px) {
-
+	.header-sect {
+		display: flex;
+		flex-direction: row;
+		padding-right: 4em;
+	}
 }
 
 /* Extra large devices (large laptops and desktops, 1200px and up) */
 @media only screen and (min-width: 1200px) {
+	.header-sect {
+		display: flex;
+		flex-direction: row;
+	}
+
 	.content {
 		margin: 0 4em;
 	}
